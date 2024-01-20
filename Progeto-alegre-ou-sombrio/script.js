0eb54e086af207d9f8b351bfcb9aed2280824c6f
wallacemt <138271229+wallacemt@users.noreply.github.com>
2024-01-20 15:36:20 -0300

    atualizei algumas coisas do progeto

---
+++ Progeto-alegre-ou-sombrio/script.js
@@ -0,0 +1,18 @@
+function mudardia(){
+    let image = document.getElementById("neutro");
+    image.src = "botdia.png"
+    let corpoPag = document.getElementById("corpo")
+    corpoPag.style.backgroundColor = 'white'
+}
+function mudarnoite(){
+    let image = document.getElementById("neutro");
+    let corpoPag = document.getElementById("corpo")
+    image.src="botnoite.png"
+    corpoPag.style.backgroundColor = 'black'
+}
+function neutrobut(){
+    let image = document.getElementById("neutro");
+    let corpoPag = document.getElementById("corpo")
+    image.src="botaoneoutro-removebg-preview.png"
+    corpoPag.style.backgroundColor = '#faeeee91'
+}
