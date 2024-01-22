c6f1e2eda3baefa8b58a0d622f3037e120ce4dad
wallacemt <138271229+wallacemt@users.noreply.github.com>
2024-01-22 19:52:18 -0300

    fiz alteracoes no progerto
    adicionei, as funcoes á programacao de mudar de imagem quando clicado no botao

--- Progeto-alegre-ou-sombrio/script.js
+++ Progeto-alegre-ou-sombrio/script.js
@@ -1,8 +1,10 @@
 function mudardia(){
     let image = document.getElementById("neutro");
-    image.src = "botdia.png"
-    let corpoPag = document.getElementById("corpo")
-    corpoPag.style.backgroundColor = 'white'
+    image.src = "botdia.png";
+    let corpoPag = document.body;
+    corpoPag.style.backgroundImage = "url('https://initiate.alphacoders.com/images/466/stretched-1920-1080-4664.jpg?2082')";
+    body.style.backgroundPosition = "center center";
+    body.style.backgroundAttachment = "fixed";
 }
 function mudarnoite(){
     let image = document.getElementById("neutro");
@@ -12,7 +14,9 @@ function mudarnoite(){
 }
 function neutrobut(){
     let image = document.getElementById("neutro");
-    let corpoPag = document.getElementById("corpo")
+    let corpoPag = document.body;
     image.src="botaoneoutro-removebg-preview.png"
-    corpoPag.style.backgroundColor = '#faeeee91'
+    corpoPag.style.backgroundImage = "url('https://images5.alphacoders.com/523/523085.jpg')";
+    body.style.backgroundPosition = "center center";
+    body.style.backgroundAttachment = "fixed";
 }
