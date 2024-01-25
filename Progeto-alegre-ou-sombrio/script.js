9015211a12712d9db45b7c5f7a74aecb1bf7f1b8
wallacemt <138271229+wallacemt@users.noreply.github.com>
2024-01-25 18:25:17 -0300

    fiz atualizações

--- Progeto-alegre-ou-sombrio/script.js
+++ Progeto-alegre-ou-sombrio/script.js
@@ -9,9 +9,9 @@ function mudardia(){
     som.play();
     somAtual = som;
     let image = document.getElementById("neutro");
-    image.src = "botdia.png";
+    image.src = "imagens/botdia.png";
     let corpoPag = document.body;
-    corpoPag.style.backgroundImage = "url('dia.jpg')";
+    corpoPag.style.backgroundImage = "url('imagens/dia.jpg')";
     body.style.backgroundPosition = "center center";
     bady.style.backgroundSize = 'cover'
     body,style.backgroundAttachment = 'fixed'
@@ -29,8 +29,8 @@ function mudarnoite(){
     somAtual = som;
     let image = document.getElementById("neutro");
     let corpoPag = document.body
-    image.src="botnoite.png"
-    corpoPag.style.backgroundImage = "url('noite.jpg')";
+    image.src="imagens/botnoite.png"
+    corpoPag.style.backgroundImage = "url('imagens/noite.jpg')";
 
     var som1 = document.getElementById("som1");
     som1.pause();
