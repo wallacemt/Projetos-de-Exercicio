5f6d60bc432855878f6a504d0acabf6c93bfde9c
wallacemt <138271229+wallacemt@users.noreply.github.com>
2024-01-30 16:12:09 -0300

    Finalizei o progeto

--- Progeto-audiobook-Javascript/script.js
+++ Progeto-audiobook-Javascript/script.js
@@ -1,15 +1,58 @@
-const botaoPlayPause = document.getElementById("play-pause")
-const audioCapitulo = document.getElementById("audio-capitulo")
+const botaoPlayPause = document.getElementById("play-pause");
+const audioCapitulo = document.getElementById("audio-capitulo");
+const botaoProximo = document.getElementById("proximo")
+const botaoAnterior= document.getElementById("anterior")
 
-const numeroCapitulos = 10
-
-let taTocando = 0
-
+const numeroCapitulos = 10;
+let taTocando = 0;
+let capituloAtual = 1;
 function tocarFaixa(){
-    audioCapitulo.play()
+    audioCapitulo.play();
+    botaoPlayPause.classList.remove('bi-play-circle');
+    botaoPlayPause.classList.add("bi-pause-circle-fill");
 }
-function pausarFaixa{
-    audioCapitulo.pause()
+function pausarFaixa(){
+    audioCapitulo.pause();
+    botaoPlayPause.classList.remove('bi-pause-circle-fill');
+    botaoPlayPause.classList.add('bi-play-circle');
 }
 
-botaoPlayPause.addEventListener('click', tocarFaixa)
+function tocarOuPausar(){
+    if( taTocando == 0) {
+        tocarFaixa();
+        taTocando += 1;
+    } else {
+        pausarFaixa();
+        taTocando = 0;
+    }
+}
+
+function proximaFaixa() {
+    if(capituloAtual == numeroCapitulos){
+        capituloAtual = 1;
+    }else{
+        capituloAtual += 1
+    }
+    audioCapitulo.src = "books/dom-casmurro/" + capituloAtual + '.mp3';
+    tocarFaixa();
+    taTocando = 1;
+    let faixaAtual = document.getElementById('faixa');
+    faixaAtual.innerText = 'Capítulo ' + capituloAtual;
+}
+
+function anteriorFaixa() {
+    if(capituloAtual == 1){
+        capituloAtual = numeroCapitulos;
+    }else{
+        capituloAtual -= 1;
+    }
+    audioCapitulo.src = "books/dom-casmurro/" + capituloAtual + '.mp3';
+    tocarFaixa();
+    taTocando = 1;
+    let faixaAtual = document.getElementById('faixa');
+    faixaAtual.innerText = 'Capítulo ' + capituloAtual;
+}
+
+botaoPlayPause.addEventListener('click', tocarOuPausar);
+botaoProximo.addEventListener('click', proximaFaixa);
+botaoAnterior.addEventListener('click', anteriorFaixa);
