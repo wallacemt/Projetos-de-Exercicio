55eecd466b67e331dd8bb1cc5bd5b5945db4b30b
wallacemt <138271229+wallacemt@users.noreply.github.com>
2024-02-14 20:31:10 -0300

    fiz alteraçoes no projeto

--- Projeto-calendario-semanal/script.js
+++ Projeto-calendario-semanal/script.js
@@ -1,27 +1,28 @@
 function butseg(){
     const subtitulosAnimesSegunda=[
-    "Kekkon Yubiwa Monogatari\n Temporada: Inverno 2024\n Estúdio: Staple Entertainment\n Status: Em Progresso\n Gêneros: Ação Ecchi Fantasia Romance", "Bucchigiri?\nTemporada: Inverno 2024\nEstúdio: MAPPA\nGêneros: Ação"];
-    const imagensAnimesSegunda=["https://www.anitube.vip/media/categories/video/32cc64407f53842cdef8a3f482a22410.jpg", "https://cdn.anicdn.net/media/categories/video/8693722045353926369c8e7ab8a5ff3e.jpg"];
+    "Kekkon Yubiwa Monogatari\n Temporada: Inverno 2024\n Estúdio: Staple Entertainment\n Status: Em Progresso\n Gêneros: Ação Ecchi Fantasia Romance", "Bucchigiri?\nTemporada: Inverno 2024\nEstúdio: MAPPA\nGêneros: Ação", "Undead Unluck\n Temporada: Outono 2023\nAutor: Yoshifumi Tozuka\nDireção: Yuuki Yase\n Estúdio: David Production\n Gêneros: Ação, Comédia, Shounen, Sobrenatural, Superpoder"];
 
+    const imagensAnimesSegunda=["https://www.anitube.vip/media/categories/video/32cc64407f53842cdef8a3f482a22410.jpg", "https://cdn.anicdn.net/media/categories/video/8693722045353926369c8e7ab8a5ff3e.jpg", "https://cdn.anicdn.net/media/categories/video/95769d5e5d2d94eb3d1f1381915f876d731.jpg"];
+    //Area de cabeçalho
     let conteudo = document.getElementById('conteudo')
     conteudo.style.display = 'inline'
     let titulo = document.getElementById('titulo')
     titulo.innerText = 'Animes de Segunda-Feira'
+
+    //Inserindo intens dentro da section
     let banner1 = document.getElementById('conteudo-1');
-    let subtitulo1 = document.getElementById('subtitulo');
+    let subtitulo1 = document.getElementById('subtitulo-1');
     subtitulo1.innerText = subtitulosAnimesSegunda[0]
     banner1.style.backgroundImage ='url(' + imagensAnimesSegunda[0] + ')';
-    
-    let banner2 = document.createElement('div');
-    banner2.className = 'conteudo-item';
-    banner2.id = 'conteudo-1'
-    let subtitulo2 = document.createElement('p');
-    subtitulo2.id = 'subtitulo'
-    subtitulo2.innerText = subtitulosAnimesSegunda[1];
-    let imagem2 = document.createElement('img');
-    imagem2.src =imagensAnimesSegunda[1];
 
-    conteudo.appendChild(banner2);
-    banner2.appendChild(imagem2);
-    banner2.appendChild(subtitulo2);
+    let banner2 = document.getElementById('conteudo-2')
+    let subtitulo2 = document.getElementById('subtitulo-2')
+    subtitulo2.innerText = subtitulosAnimesSegunda[1]
+    banner2.style.backgroundImage = 'url(' + imagensAnimesSegunda[1] + ')';
+
+    let banner3 = document.getElementById('conteudo-3')
+    let subtitulo3 = document.getElementById('subtitulo-3')
+    subtitulo3.innerText = subtitulosAnimesSegunda[2]
+    banner3.style.backgroundImage = 'url('+ imagensAnimesSegunda[2] + ')';
+
 }
