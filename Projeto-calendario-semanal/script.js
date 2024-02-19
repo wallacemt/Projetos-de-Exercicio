bbe0da8f79408e538de9960d104bfb49dda39a9d
wallacemt <138271229+wallacemt@users.noreply.github.com>
2024-02-19 20:16:22 -0300

    Fiz alteraçoes e adicioneis mais funcionalidades.

--- Projeto-calendario-semanal/script.js
+++ Projeto-calendario-semanal/script.js
@@ -13,6 +13,18 @@ var subtitulo3 = document.getElementById('subtitulo-3');
 var link3 = document.getElementById('link-3');
 
 var taAparecendo = 0
+
+function menu(){
+    let menu = document.getElementById('menu')
+    if(menu.style.display == 'block'){
+        menu.style.display = 'none'
+        conteudo.style.display = 'none'
+    }else{
+        menu.style.display = 'block'
+        conteudo.style.display = 'none'
+    }
+}
+
 function butseg(){
     const subtitulosAnimesSegunda=[
     "Kekkon Yubiwa Monogatari\n Temporada: Inverno 2024\n Estúdio: Staple Entertainment\n Status: Em Progresso\n Gêneros: Ação Ecchi Fantasia Romance", "Bucchigiri?\nTemporada: Inverno 2024\nEstúdio: MAPPA\nGêneros: Ação", "Undead Unluck\n Temporada: Outono 2023\nAutor: Yoshifumi Tozuka\nDireção: Yuuki Yase\n Estúdio: David Production\n Gêneros: Ação, Comédia, Shounen, Sobrenatural, Superpoder"];
@@ -37,10 +49,10 @@ function butseg(){
         
         subtitulo3.innerText = subtitulosAnimesSegunda[2]
         banner3.style.backgroundImage = 'url('+ imagensAnimesSegunda[2] + ')';
+        banner3.style.display = 'block'
         link3.href = linkAnimesSegunda[2];
 
         conteudo.style.display = 'block'
-
         if(window.innerWidth <= 800 && taAparecendo === 0){
             let menu = document.getElementById('menu')
             menu.style.display = 'none'
@@ -53,13 +65,110 @@ function butseg(){
     
 }
 
-function menu(){
-    let menu = document.getElementById('menu')
-    if(menu.style.display == 'block'){
-        menu.style.display = 'none'
-        conteudo.style.display = 'none'
+function butter(){
+    const subtitulosAnimesTerca = ["Boku no Kokoro no Yabai Yatsu Season 2\nTemporada: Inverno 2024\nEstúdio: Shin-Ei Animation\nGêneros: Comédia Romance", "Urusei Yatsura (2022) 2nd Season\nEstúdio: David Production\nStatus: Em Progresso\nGêneros: Comédia Romance Sci-Fi"];
+    const imagensAnimesTerca = ["https://www.anitube.vip/media/categories/video/ffdf04427cfea3528928482d8c9ee6d4.jpg", "https://www.anitube.vip/media/categories/video/afd0deef11d5a3a4890d1f0cb6902092.jpg"];
+    const linkAnimesTerca = ["https://www.anitube.vip/anime/boku-no-kokoro-no-yabai-yatsu-season-2", "https://www.anitube.vip/anime/urusei-yatsura-2022-2nd-season"]
+    if(taAparecendo == 0){
+        //Area de cabeçalho
+        conteudo.style.display = 'inline'
+        let titulo = document.getElementById('titulo')
+        titulo.innerText = 'Animes de Terça-Feira'
+    
+        //Inserindo intens dentro da section
+        subtitulo1.innerText = subtitulosAnimesTerca[0]
+        banner1.style.backgroundImage ='url(' + imagensAnimesTerca[0] + ')';
+        link1.href = linkAnimesTerca[0];
+    
+        subtitulo2.innerText = subtitulosAnimesTerca[1]
+        banner2.style.backgroundImage = 'url(' + imagensAnimesTerca[1] + ')';
+        link2.href = linkAnimesTerca[1];
+
+        conteudo.style.display = 'block'
+
+        if(window.innerWidth <= 800 && taAparecendo === 0){
+            let menu = document.getElementById('menu')
+            menu.style.display = 'none'
+        }
+
+        banner3.style.display = 'none'
+        subtitulo3.innerText = ''
+        taAparecendo +=1
     }else{
-        menu.style.display = 'block'
         conteudo.style.display = 'none'
+        taAparecendo = 0
+        
+    }
+}
+
+function butqua(){
+    const subtitulosAnimesQuarta = ["Estúdio: Seven Arcs\nStatus: Em Progresso\nGêneros: Ação Ecchi Fantasia", "Autor: Ryousuke Fuji e Katarina\nDireção: Toshiyuki Kubooka\nEstúdio: C2C\nGêneros: Ação Aventura Fantasia Jogos Shounen"];
+    const imagensAnimesQuarta = ["https://www.anitube.vip/media/categories/video/5d9539763ee2a79091013e3792bbf267.jpg", "https://www.anitube.vip/media/categories/video/e5bb61f161dadfca4ebe6d6016a94462940.jpg"];
+    const linkAnimesQuarta = ['https://www.anitube.vip/anime/mato-seihei-no-slave', 'https://www.anitube.vip/anime/shangri-la-frontier-kusoge-hunter-kamige-ni-idoman-to-su'];
+    if(taAparecendo == 0){
+        //Area de cabeçalho
+        conteudo.style.display = 'inline'
+        let titulo = document.getElementById('titulo')
+        titulo.innerText = 'Animes de Quarta-Feira'
+    
+        //Inserindo intens dentro da section
+        subtitulo1.innerText = subtitulosAnimesQuarta[0]
+        banner1.style.backgroundImage ='url(' + imagensAnimesQuarta[0] + ')';
+        link1.href = linkAnimesQuarta[0];
+    
+        subtitulo2.innerText = subtitulosAnimesQuarta[1]
+        banner2.style.backgroundImage = 'url(' + imagensAnimesQuarta[1] + ')';
+        link2.href = linkAnimesQuarta[1];
+
+        conteudo.style.display = 'block'
+
+        if(window.innerWidth <= 800 && taAparecendo === 0){
+            let menu = document.getElementById('menu')
+            menu.style.display = 'none'
+        }
+
+        banner3.style.display = 'none'
+        subtitulo3.innerText = ''
+        taAparecendo +=1
+    }else{
+        conteudo.style.display = 'none'
+        taAparecendo = 0
+    }
+}
+
+function butqui(){
+    const subtitulosAnimesQuinta = ['Akuyaku Reijou Level 99: Watashi wa Ura-Boss desu ga Maou dewa Arimasen\nEstúdio: Jumondo\nGêneros: Fantasia', 'Majo to Yajuu\nEstúdio: Yokohama Animation Lab\nGêneros: Ação Fantasia', 'Metallic Rouge\nEstúdio: Bones\nGêneros: Ação Sci-Fi'];
+    const imagensAnimesQuinta = ['https://www.anitube.vip/media/categories/video/984d09774965045ada1662baec5abba4.jpg', 'https://www.anitube.vip/media/categories/video/dcc0d0b608ff9782e5495b222be50e00.jpg', 'https://www.anitube.vip/media/categories/video/97d5400491dfd0cdbbbc843db3e7e53b.jpg'];
+    const linkAnimesQuinta = ['https://www.anitube.vip/anime/akuyaku-reijou-level-99-watashi-wa-ura-boss-desu-ga-maou-dewa-arimasen', 'https://www.anitube.vip/anime/majo-to-yajuu', 'https://www.anitube.vip/anime/metallic-rouge'];
+    if(taAparecendo == 0){
+        //Area de cabeçalho
+        conteudo.style.display = 'inline'
+        let titulo = document.getElementById('titulo')
+        titulo.innerText = 'Animes de Quinta-Feira'
+    
+        //Inserindo intens dentro da section
+        subtitulo1.innerText = subtitulosAnimesQuinta[0]
+        banner1.style.backgroundImage ='url(' + imagensAnimesQuinta[0] + ')';
+        link1.href = linkAnimesQuinta[0];
+    
+        subtitulo2.innerText = subtitulosAnimesQuinta[1]
+        banner2.style.backgroundImage = 'url(' + imagensAnimesQuinta[1] + ')';
+        link2.href = linkAnimesQuinta[1];
+
+        subtitulo3.innerText = subtitulosAnimesQuinta[2]
+        banner3.style.backgroundImage = 'url('+ imagensAnimesQuinta[2] + ')';
+        banner3.style.display = 'block'
+        link3.href = linkAnimesQuinta[2];
+
+        conteudo.style.display = 'block'
+
+        if(window.innerWidth <= 800 && taAparecendo === 0){
+            let menu = document.getElementById('menu')
+            menu.style.display = 'none'
+        }
+        taAparecendo +=1
+    }else{
+        conteudo.style.display = 'none'
+        taAparecendo = 0
     }
 }
