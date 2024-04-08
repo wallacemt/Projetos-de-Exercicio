5b25ef62c4e1da899d8f6a0b1c989d32bccfc18b
wallacemt <138271229+wallacemt@users.noreply.github.com>
2024-04-08 12:21:21 -0300

    aula 8

---
+++ aula-programacao-grautc/aula-8/script.js
@@ -0,0 +1,9 @@
+// let nome = window.prompt("Qual seu nome?")
+// window.alert(`Bem Vindo ${nome}`)
+
+let n1 = Number(window.prompt("Digite um numero: "));
+
+let n2 = Number(window.prompt("Digite outro numero: "));
+
+
+let s = window.alert(`A Soma entre ${n1} + ${n2} é ${n1 + n2}`);
