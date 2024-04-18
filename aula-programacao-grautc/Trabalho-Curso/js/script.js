dbba489e9ab7e094951a69f596b244573b6d422e
wallacemt <138271229+wallacemt@users.noreply.github.com>
2024-04-18 15:51:30 -0300

    Adicioneis outras paginas

--- aula-programacao-grautc/Trabalho-Curso/js/script.js
+++
@@ -1,8 +0,0 @@
-function menu(){
-    let itemMenu = document.querySelector("#navbar-items li")
-
-    itemMenu.style.display = 'inline-block'
-    if(itemMenu.style.display == 'none'){
-        console.log(itemMenu);
-    }
-}
